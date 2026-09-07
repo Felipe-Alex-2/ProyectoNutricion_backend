5f63c70880286467ab04e6a7079a0083f40b35b8
Felipe-Alex-2 <alexeduardofelipe2@gmail.com>
2026-09-06 20:33:51 -0400

    commit de la salvacion

--- app/database.py
+++ app/database.py
@@ -4,7 +4,9 @@ from app.config import settings
 
 db_url = settings.DATABASE_URL
 if db_url.startswith("postgres://"):
-    db_url = db_url.replace("postgres://", "postgresql://", 1)
+    db_url = db_url.replace("postgres://", "postgresql+psycopg2://", 1)
+elif db_url.startswith("postgresql://") and not db_url.startswith("postgresql+"):
+    db_url = db_url.replace("postgresql://", "postgresql+psycopg2://", 1)
 
 connect_args = {}
 if db_url.startswith("sqlite"):
